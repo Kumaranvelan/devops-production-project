aa19e26b93f6dc5ef1f6306572f34bfdcfa00e7d
Kumaranvelan <kumaravelan9721@gmail.com>
2026-03-17 02:42:07 +0000

    updated Jenkinsfile, terraform files, added gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,108 @@
 provider "aws" {
-  region = "us-east-1"
-  }
+  region = var.region
+}
 
-resource "aws_instance" "devops_server" {
-  ami = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
-
-  key_name = "devops-key"
-  user-data = file("user-data.sh")
+# VPC
+resource "aws_vpc" "devops_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-  Name = "DevopsServer"
+    Name = "devops-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "devops_subnet" {
+  vpc_id                  = aws_vpc.devops_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "devops-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "devops_igw" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "devops_rt" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.devops_igw.id
   }
 
+  tags = {
+    Name = "devops-rt"
   }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "devops_rta" {
+  subnet_id      = aws_subnet.devops_subnet.id
+  route_table_id = aws_route_table.devops_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-sg"
+  description = "Allow SSH, HTTP, Jenkins"
+  vpc_id      = aws_vpc.devops_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "devops_server" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.devops_subnet.id
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+  user_data              = file("user-data.sh")
+
+  tags = {
+    Name = "DevopsServer"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
-variable "region"{
- default = "us-east-1"
- }
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-0ec10929233384c7f"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "devops-key"
+}
